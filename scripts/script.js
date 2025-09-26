fcdd65273d64a9ae18c3a62659361f308a9864aa
Levi Arcanjo <murilolevi0@gmail.com>
2025-09-26 11:02:28 -0300

    conserto do outro negocio

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-var service = document.querySelector(".service");
-
-service.addEventListener("click", () => {
-    window.open("https://wa.me/5581996609690?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os.", "_blank");
-});
